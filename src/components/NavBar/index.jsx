8bc6a11a3ac5e0fe156be5cdf12edeeb61d38104
August <august.gros@gmail.com>
2023-05-03 12:11:39 +0200

    upgrade navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { View, TouchableOpacity, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import {style} from './style'
+import { View, TouchableOpacity, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { style } from "./style";
 
-export const NavBar = ({campaignId}) => {
+export const NavBar = ({ campaignId }) => {
   const navigation = useNavigation();
 
   return (
     <View style={style.container}>
-      <TouchableOpacity onPress={() => navigation.navigate('Map', {campaignId: campaignId})}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Map", { campaignId: campaignId })}
+      >
         <Text>Map</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Crew', {campaignId: campaignId})}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Crew", { campaignId: campaignId })}
+      >
         <Text>Crew</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+      <TouchableOpacity onPress={() => console.log("dice")}>
+        <Text>Dice</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => console.log("timer")}>
+        <Text>Timer</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => console.log("settings")}>
         <Text>Options</Text>
       </TouchableOpacity>
     </View>
--- src/components/NavBar/style.jsx
+++ src/components/NavBar/style.jsx
@@ -1,17 +1,17 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
-    container: {
-        position: "absolute",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        height: '50px',
-        backgroundColor: "lightgrey",
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        paddingVertical: 0,
-        paddingHorizontal: 10,
-    }
-})  
+  container: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: "50px",
+    backgroundColor: "lightgrey",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingVertical: 0,
+    paddingHorizontal: 10,
+  },
+});
