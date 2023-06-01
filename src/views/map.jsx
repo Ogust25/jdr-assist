a2b044acaf62cd866e5ebea696830a655b419ed3
August <august.gros@gmail.com>
2023-06-01 13:32:01 +0200

    fix campaign name

--- src/views/map.jsx
+++ src/views/map.jsx
@@ -1,26 +1,33 @@
-import React from "react";
-import { View, Image, TouchableOpacity } from "react-native";
-import { CustomText } from "../components/CustomText";
-import { useRoute } from "@react-navigation/native";
-import { NavBar } from "../components/NavBar";
-import { style } from "../style/map";
-import logo from "../../public/assets/images/logo.png";
+import React, { useState, useEffect } from 'react';
+import { View, Image } from 'react-native';
+import { CustomText } from '../components/CustomText';
+import { useRoute } from '@react-navigation/native';
+import { NavBar } from '../components/NavBar';
+import { style } from '../style/map';
+import logo from '../../public/assets/images/logo.png';
+import { db, collection, doc, getDoc } from '../firebase/config';
 
 export const Map = () => {
-  const route = useRoute();
-  const { campaignId } = route.params;
+	const route = useRoute();
+	const { campaignId } = route.params;
+	const [campaign, setCampaign] = useState({ name: '' });
 
-  return (
-    <View style={style.container}>
-      <CustomText>Menu Campagnes {campaignId + 1}</CustomText>
+	const getCampaign = async () => {
+		const querySnapshot = await getDoc(
+			doc(collection(db, 'campaign'), campaignId),
+		);
+		setCampaign(querySnapshot.data());
+	};
 
-      <Image
-        source={logo}
-        style={style.img}
-        onPress={() => console.log("ougabouga")}
-      />
+	useEffect(() => {
+		getCampaign();
+	}, []);
 
-      <NavBar campaignId={campaignId} />
-    </View>
-  );
+	return (
+		<View style={style.container}>
+			<CustomText>Menu Campagnes {campaign.name}</CustomText>
+			<Image source={logo} style={style.img} />
+			<NavBar campaignId={campaignId} />
+		</View>
+	);
 };
